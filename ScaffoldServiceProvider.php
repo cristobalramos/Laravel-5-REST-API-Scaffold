a7b19243f729755f24e5d5161a16f45d0ee6c60c
Cristóbal Ramos Merino <cristobalramosmerino@gmail.com>
2017-12-21 07:40:38 +0000

    First commit

---
+++ ScaffoldServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ramosmerino\Scaffold;
+
+use Illuminate\Support\ServiceProvider;
+
+class GeneratorsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerCreate();
+    }
+
+    /**
+     * Register the scaffold:create generator.
+     */
+    private function registerCreate()
+    {
+        $this->app->singleton('command.ramosmerino.create', function ($app) {
+            return $app['Ramosmerino\Scaffold\Commands\ScaffoldCreateCommand'];
+        });
+
+        $this->commands('command.ramosmerino.create');
+    }
+}
